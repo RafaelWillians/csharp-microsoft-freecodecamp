d298091e63ff3f9f94899eeb7e99e48149fe4e9d
RafaelWillians <rafael.willians@outlook.com>
2023-09-22 23:17:56 -0300

    Inseridos comentários no código

--- 09-MatrizForeach/Program.cs
+++ 09-MatrizForeach/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
-
+// Inicializando os vetores das notas e dos nomes dos alunos.
 string[] nomeAluno = new string[] {"Sophia", "Andrew", "Emma" , "Logan"};
  
 int[] sophiaNota = new int[] {90, 86, 87, 98, 100};
 int[] andrewNota = new int[] {92, 89, 81, 96 , 90};
 int[] emmaNota = new int[] {90, 85, 87, 98, 68};
 int[] loganNota = new int[] {90, 95, 87, 88, 96};
+
+/* 
+    Declaração das variáveis da soma, média e letras das notas, 
+    além do índice para percorrer pelos vetores usando foreach (sem usar for no momento)
+    e a quantidadeNota para a divisão da média.
+*/
 
 int[] somaNota = new int [4];
 
@@ -17,12 +23,23 @@ string[] letraNota = new string[4];
 int indice = 0;
 int quantidadeNota = 5;
 
-Console.WriteLine("Student\t\tGrade\n");
-
+// Preencho o vetor para somar as notas dos alunos.
 somaNota[0] = sophiaNota.Sum();
 somaNota[1] = andrewNota.Sum();
 somaNota[2] = emmaNota.Sum();
 somaNota[3] = loganNota.Sum();
+
+// Cabeçalho do output
+Console.WriteLine("Student\t\tGrade\n");
+
+
+/*
+    Para cada nota, é calculada a média e atribuída a letra da nota correspondente.
+    Necessário comparar explicitamente mediaNota como int, pois caso contrário
+    resultará em erro em algumas notas e/ou letraNota não sairá correspondente a
+    tabela do exercício.
+    Por enquanto, sem usar for ou switch-case.    
+*/
 
 foreach(int nota in somaNota)
 {
