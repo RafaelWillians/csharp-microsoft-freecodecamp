07614443965d7f681f92178f0c4abc2e204e895a
RafaelWillians <rafael.willians@outlook.com>
2023-09-24 16:40:37 -0300

    Criando exercícios para término do desafio e pasta Extra

--- 09-B-MatrizForeach-CreditoExtra/Program.cs
+++ 09-B-MatrizForeach-CreditoExtra/Program.cs
@@ -1,5 +1,4 @@
-﻿// initialize variables - graded assignments
-int examAssignments = 5;
+﻿int examAssignments = 5;
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
@@ -13,22 +12,39 @@ int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
 // Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
-// Student names
-string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
-
 int[] studentScores = new int[10];
 
 string currentStudentLetterGrade = "";
 
-// Write the Report Header to the console
-Console.WriteLine("Student\t\tGrade\n");
 
+// NOVO
+// decimal pontoCreditoExtra = 0;
+// decimal mediaCreditoExtra = 0;
+
+
+
+/* 
+Console.WriteLine("Student\t\tGrade\n");
+ */
+
+
+Console.WriteLine($"Student\t\tExam Score\tOverall\tGrade\t\tExtra Credit\n");
+
+
+/*
+The outer foreach loop is used to:
+- iterate through student names 
+- assign a student's grades to the studentScores array
+- sum assignment scores (inner foreach loop)
+- calculate numeric and letter grade
+- write the score report information
+*/
 foreach (string name in studentNames)
 {
     string currentStudent = name;
 
     if (currentStudent == "Sophia")
-       studentScores = sophiaScores;
+        studentScores = sophiaScores;
 
     else if (currentStudent == "Andrew")
         studentScores = andrewScores;
@@ -38,87 +54,82 @@ foreach (string name in studentNames)
 
     else if (currentStudent == "Logan")
         studentScores = loganScores;
-    
+
     else if (currentStudent == "Becky")
     studentScores = beckyScores;
+
     else if (currentStudent == "Chris")
         studentScores = chrisScores;
+
     else if (currentStudent == "Eric")
         studentScores = ericScores;
+
     else if (currentStudent == "Gregor")
         studentScores = gregorScores;
+
     else
         continue;
 
-    // initialize/reset the sum of scored assignments
     int sumAssignmentScores = 0;
-
-    // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
-
-    // initialize/reset a counter for the number of assignment 
     int gradedAssignments = 0;
 
-    // loop through the scores array and complete calculations for currentStudent
+
+    /* 
+    the inner foreach loop sums assignment scores
+    extra credit assignments are worth 10% of an exam score
+    */
     foreach (int score in studentScores)
     {
-        // increment the assignment counter
         gradedAssignments += 1;
 
         if (gradedAssignments <= examAssignments)
-            // add the exam score to the sum
             sumAssignmentScores += score;
-
         else
-            // add the extra credit points to the sum - bonus points equal to 10% of an exam score
-            sumAssignmentScores += score / 10;
+            sumAssignmentScores += score / 10; //CALCULAR AQUI OU FORA DO FOREACH
     }
 
     currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
 
     if (currentStudentGrade >= 97)
         currentStudentLetterGrade = "A+";
-
     else if (currentStudentGrade >= 93)
         currentStudentLetterGrade = "A";
-
     else if (currentStudentGrade >= 90)
         currentStudentLetterGrade = "A-";
-
     else if (currentStudentGrade >= 87)
         currentStudentLetterGrade = "B+";
-
     else if (currentStudentGrade >= 83)
         currentStudentLetterGrade = "B";
-
     else if (currentStudentGrade >= 80)
         currentStudentLetterGrade = "B-";
-
     else if (currentStudentGrade >= 77)
         currentStudentLetterGrade = "C+";
-
     else if (currentStudentGrade >= 73)
         currentStudentLetterGrade = "C";
-
     else if (currentStudentGrade >= 70)
         currentStudentLetterGrade = "C-";
-
     else if (currentStudentGrade >= 67)
         currentStudentLetterGrade = "D+";
-
     else if (currentStudentGrade >= 63)
         currentStudentLetterGrade = "D";
-
     else if (currentStudentGrade >= 60)
         currentStudentLetterGrade = "D-";
-
     else
         currentStudentLetterGrade = "F";
 
-    //Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+    // Student         Grade
+    // Sophia:         92.2    A-
+    
     Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
 
 // required for running in VS Code (keeps the Output windows open to view results)
+Console.WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
+
+
+
+Console.ReadLine();
 Console.WriteLine("\n\rPress the Enter key to continue");
 Console.ReadLine();
---
+++ Extra/FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+// Crio map, para preencher tanto com os números, quanto com FizzBuzz.
+Dictionary<int, string> map = new Dictionary<int, string>();
+
+// Apenas para input dos valores. Pode ser aprimorado para apenas usarmos o valor de i.
+for(int i =0; i<=100; i++)
+{
+    string fizzBuzz;
+    fizzBuzz = (i%3==0 && i%5==0) ? "FizzBuzz" : 
+        (i%5==0) ? "Buzz" : 
+            (i%3==0) ? "Fizz" : i.ToString();
+    map.Add(i,fizzBuzz);
+}
+
+// Apenas para saída dos valores.
+foreach(var paresMapa in map)
+{
+    Console.WriteLine($"{paresMapa}");    
+}
+
+
+
