1174e1201b97d819d9c962416cd2da98b88ebcc9
RafaelWillians <rafael.willians@outlook.com>
2023-10-19 18:50:21 -0300

    Iniciado módulo do-while

---
+++ 21-DoWhile/Program.cs
@@ -0,0 +1,61 @@
+﻿/* 
+// Exemplo 1
+Random random = new Random();
+int current = 0;
+
+do
+{
+    // Sempre será executado esse bloco pelo menos uma vez.
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+ */
+
+
+/* 
+// Exemplo 2
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+
+// do
+// {
+//     current = random.Next(1, 11);
+//     Console.WriteLine(current);
+// } while (current != 7);
+
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+
+ */
+
+
+// Exemplo 3
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+*/
+
