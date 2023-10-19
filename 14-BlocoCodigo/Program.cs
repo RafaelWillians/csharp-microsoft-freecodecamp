d1f9419d8af62419e6a6d020efced88b67711964
RafaelWillians <rafael.willians@outlook.com>
2023-10-19 12:19:22 -0300

    Iniciado módulo de bloco de códigos

---
+++ 14-BlocoCodigo/Program.cs
@@ -0,0 +1,9 @@
+﻿bool flag = true;
+if (flag)
+{
+    int value = 10;
+    Console.WriteLine($"Inside of code block: {value}");
+}
+
+/* Dará erro de compilação a linha abaixo. Value está apenas dentro do escopo do if acima.
+Console.WriteLine($"Outside of code block: {value}"); */
