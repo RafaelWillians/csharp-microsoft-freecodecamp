cb56b832618d1f88638aef53ac4e02247eba6b57
RafaelWillians <rafael.willians@outlook.com>
2023-10-19 12:12:06 -0300

    Concluindo módulo de operador condicional

---
+++ 12-CoinFlip/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Random randomNumber = new Random();
+int coinFlip = randomNumber.Next(1,100);
+
+Console.WriteLine($"{(coinFlip % 2 == 0 ? "Heads" : "Tails")}");
+
+// Apenas para mostrar o valor da variável coinFlip.
+// Console.WriteLine(coinFlip);
---
+++ 13-PermissionLevel/Program.cs
@@ -0,0 +1,4 @@
+﻿string permission = "Admin|Manager";
+int level = 55;
+
+Console.WriteLine($"{(permission.Contains("Admin") && level > 55 ? "Welcome, Super Admin user." : permission.Contains("Admin") && level <= 55 ? "Welcome, Admin user." : permission.Contains("Manager") && level >= 20 ? "Contact an Admin for access." : permission.Contains("Manager") && level < 20 ? "You do not have sufficient privileges." : "You do not have sufficient privileges." )}");
