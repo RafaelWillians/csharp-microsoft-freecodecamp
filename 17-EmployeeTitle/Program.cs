f28ae1a1cd7cede32cf32e2aca6f0a6d84353e98
RafaelWillians <rafael.willians@outlook.com>
2023-10-19 13:56:48 -0300

    Finalizado modulo de switch-case

---
+++ 17-EmployeeTitle/Program.cs
@@ -0,0 +1,24 @@
+﻿int employeeLevel = 100;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    //É possível determinar da forma abaixo, para que dois cases distintos executem a mesma ação.
+    case 100:
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
