328af0bfac635fdf8e7c5ed490be98ae2fc0cbf6
RafaelWillians <rafael.willians@outlook.com>
2023-10-19 13:37:46 -0300

    Adicionado exemplo switch-case

---
+++ 16-SwitchCaseExemplo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string fruit = "CHERRY";
+
+// Switch, normalizando a string, para sempre corresponder a caixa baixa dos cases abaixo.
+switch (fruit.ToLower())
+{
+    case "apple":
+        Console.WriteLine($"App will display information for apple.");
+        break;
+
+    case "banana":
+        Console.WriteLine($"App will display information for banana.");
+        break;
+
+    case "cherry":
+        Console.WriteLine($"App will display information for cherry.");
+        break;
+}
