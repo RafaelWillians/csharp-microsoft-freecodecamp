28cf6cc22d97873b110e5f79e23dce987e8940d3
RafaelWillians <rafael.willians@outlook.com>
2023-10-19 20:59:08 -0300

    Incluso exercício 1 de do-while

---
+++ 23-ChecarInputUsuario/Program.cs
@@ -0,0 +1,35 @@
+﻿int numericValue = 0;
+
+string? readResult;
+
+bool validEntry = false;
+bool validNumber = false;
+
+
+Console.WriteLine("Digite um número inteiro entre 5 e 10: ");
+do
+{
+    readResult = Console.ReadLine();
+    if (readResult != null)
+    {
+        validNumber = int.TryParse(readResult, out numericValue);
+
+        if (validNumber)
+        {
+            if(numericValue >= 5 && numericValue <= 10)
+            {
+                validEntry = true;
+                Console.WriteLine($"O número digitado {numericValue} foi aceito com sucesso.");
+            }
+            else
+            {
+                Console.WriteLine("Dados inválidos. Tente novamente.");
+            }
+                        
+        }
+        else
+        {
+            Console.WriteLine("Dados inválidos. Tente novamente.");
+        }
+    }
+} while (validEntry == false);
