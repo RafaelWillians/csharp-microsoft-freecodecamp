db5f16b41ae14ea188b720e9da68f104876ecc07
RafaelWillians <rafael.willians@outlook.com>
2023-10-19 14:37:25 -0300

    Concluindo módulo For

---
+++ 19-For/Program.cs
@@ -0,0 +1,40 @@
+﻿/* for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    // Aqui quebra o bloco, depois que i chega a 7.
+    if (i == 7) break;
+}
+
+Console.WriteLine();
+
+// Exemplo 2
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+ */
+
+// Exemplo 3
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David") names[i] = "Sammy";
+}
+
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
---
+++ 20-ExercicioFizzBuzz/Program.cs
@@ -0,0 +1,11 @@
+﻿// Código otimizado já feito anteriormente no diretório Extra\FizzBuzz, com modificações para mostrar todos os números na saída.
+
+for(int i =0; i<=100; i++)
+{
+    // A linha abaixo vai servir para checar se é divisível por 3 ou 5 e atribuir FizzBuzz, sem precisar de if's.
+    string fizzBuzz = (i%3==0 && i%5==0) ? " - FizzBuzz" : 
+        (i%5==0) ? " - Buzz" : 
+            (i%3==0) ? " - Fizz" : "";
+
+    Console.WriteLine($"{i}{fizzBuzz}");
+}
