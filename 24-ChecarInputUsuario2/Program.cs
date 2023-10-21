83e0b9d97e3c935c4b107db34f06c3698a99886f
RafaelWillians <rafael.willians@outlook.com>
2023-10-21 15:18:28 -0300

    Finalizando exercicios modulo do-while

--- 24-ChecarInputUsuario2/Program.cs
+++ 24-ChecarInputUsuario2/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? inputUsuario = "";
+string? funcao = "";
+char[] charsTrim = {' ', '.', ',', ';'};
+bool validarInput = false;
 
+do
+{
+    Console.WriteLine("Digitar função (administrador, gerente, usuario): ");
+
+    inputUsuario = Console.ReadLine();
+    funcao = inputUsuario!.Trim(charsTrim);
+    funcao = funcao.ToLower();
+    
+    switch (funcao)
+    {
+        case "administrador": 
+        case "gerente":
+        case "usuario":
+            Console.WriteLine($"Função de ({funcao}) aceita com sucesso.");
+            validarInput = true;
+        break;
+    
+        default: 
+            Console.WriteLine("Entrada inválida. Digite novamente.\n");
+            validarInput = false;
+        break;
+
+    }    
+
+} while (validarInput == false);
--- 25-TrimString/Program.cs
+++ 25-TrimString/Program.cs
@@ -1,4 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string? inputSaida;
-inputSaida = Console.ReadLine();
+﻿string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+string textoCortado = "";
+int periodLocation = 0;
+int inicioTexto = 0;
+int tamanhoSubtexto = 0;
+bool checarRemocaoPonto = false;
+int tamanhoTexto = 0;
+
+
+foreach(string myString in myStrings)
+{
+    textoCortado = myString;
+    tamanhoTexto = textoCortado.Length;
+    inicioTexto = 0;
+
+    do
+    {        
+        periodLocation = textoCortado.IndexOf(".");
+
+        if(periodLocation >= 0)
+        {
+            
+            tamanhoSubtexto = periodLocation - inicioTexto;
+            textoCortado = textoCortado.Remove(periodLocation, 1);
+            Console.WriteLine($"{textoCortado.Substring(inicioTexto, tamanhoSubtexto)}");
+            inicioTexto = periodLocation+1;
+            checarRemocaoPonto = true;
+            tamanhoTexto = tamanhoTexto - tamanhoSubtexto - 2;
+        }
+        if(checarRemocaoPonto == false && inicioTexto == 0)
+        {
+            Console.WriteLine($"{myString}");
+        }
+        if(periodLocation < 0 && checarRemocaoPonto == true)
+        {            
+            tamanhoSubtexto = tamanhoTexto;
+            Console.WriteLine($"{textoCortado.Substring(inicioTexto, tamanhoSubtexto)}");
+        }  
+
+    } while(periodLocation >= 0);
+
+}
